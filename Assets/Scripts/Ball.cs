7ded5b2d438a6263b1ed588058f7c1b096b6234e
Samantha <labarbes2@hawkmail.newpaltz.edu>
2017-03-13 10:54:19 -0400

    Beginning of project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Vector2 startingVelocity = new Vector2(15, -21);
+    public Vector3 startingPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+        GetComponent<Rigidbody2D>().velocity = startingVelocity; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
