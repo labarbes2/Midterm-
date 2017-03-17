29de326415e57200062e678aec632dcd2e7f63f1
Samantha <labarbes2@hawkmail.newpaltz.edu>
2017-03-16 23:51:38 -0400

    Final project: all 3 levels with requirements

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Ball : MonoBehaviour {
-    public Vector2 startingVelocity = new Vector2(15, -21);
-    public Vector3 startingPosition;
+using UnityEngine.UI;
 
 
-	// Use this for initialization
-	void Start () {
+public class Ball : MonoBehaviour { 
+
+    public Vector2 startingVelocity = new Vector2(15, -20);
+    private Vector3 startingPosition;
+    public GameObject gameOverSign;
+    public GameObject youWinSign;
+    public Text livesValue;
+    public Text pointsValue;
+
+    int lives = 3;
+    int points = 0;
+
+    void Start() { 
         startingPosition = transform.position;
-        GetComponent<Rigidbody2D>().velocity = startingVelocity; 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        livesValue.text = lives.ToString();
+    }
+
+
+    void Update() { 
+        if (transform.position.y < -3.7f)
+        {
+            GetOut();
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
+    }
+    void GetOut()
+    {
+        Debug.Log("You are out");
+        lives = lives - 1;
+        livesValue.text = lives.ToString();
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+
+        if (lives == 0)
+        {
+            DoGameOver();
+        }
+    }
+
+    void DoGameOver()
+    {
+        gameOverSign.SetActive(true);
+    }
+    public void YouBrokeABrick()
+    {
+        points += 1;
+        pointsValue.text = points.ToString();
+        var bricksLeft = FindObjectsOfType<Brick>().Length;
+        if (bricksLeft == 0)
+        {
+            youWinSign.SetActive(true);
+        }
+    }
 }
--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brick : MonoBehaviour {
+public class Brick : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int health = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        health -= 1; // health -= 1 is the same as saying health = health - 1
+        Debug.Log(health);
+
+        if (health == 0)
+        {
+            gameObject.SetActive(false);
+            FindObjectOfType<Ball>().YouBrokeABrick();
+        }
+
+    }
 }
